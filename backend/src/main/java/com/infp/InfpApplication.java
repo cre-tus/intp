c2b5e967a7ba764f99b3b97c83ff9c4ca93bf19d
JINYOUNG YUN <pineappleboy@tukorea.ac.kr>
2026-01-08 15:56:37 +0900

    초기 세팅

---
+++ backend/src/main/java/com/infp/InfpApplication.java
@@ -0,0 +1,13 @@
+package com.infp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfpApplication.class, args);
+    }
+
+}
